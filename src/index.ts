570f407bac1223a63c3b6778217b4b6d89228a53
valexdevelopers <egeregav@gmail.com>
2025-01-05 12:00:00 +0100

    Project initalization

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import { loadRoutes } from './routes';
+// import { errorHandler } from './middlewares/errorHandler';
+// import { rateLimiter } from './middlewares/rateLimiter';
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+// app.use(rateLimiter); // apply global rate limiter
+
+// Routes
+loadRoutes(app);
+
+// Error handling
+// app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
