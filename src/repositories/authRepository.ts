e32112bed3dd11fb413375abfb92db08b0501ab5
Virtue Egerega <egeregav@gmail.com>
2025-06-15 13:16:00 +0100

    Update: implemented crud operations for auth repositories

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,13 +1,14 @@
-
-import { PrismaClient, Prisma, $Enums } from "../../prisma/generated/client"
+import { PrismaClient, Prisma, $Enums} from "../../@prisma/client";
 import {prisma} from './db'
 import { OAuth2Client } from 'google-auth-library'
-import {  IAuthRepository } from "../utils/interfaces/account.interface"
+import {  FindManyUsers, IAuthRepository } from "../utils/interfaces/account.interface"
 import { AuthError } from "../utils/errorhandlers"
 import { AuthErrorCode } from "../utils/enums"
 import { CreateAuthProviderDto, CreateUserDto } from "../utils/dtos/user/create-user.dto"
 import { LoginUserDto } from "../utils/dtos/user/login-user.dto"
 import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken"
+import { UserDto } from "../utils/dtos/user/user.dto"
 
 /**
  * Repository for managing account relations.
@@ -27,12 +28,14 @@ import bcrypt from 'bcrypt';
  * @see UserModel
  */
 
-class AuthRepository {
+export default class AuthRepository {
     private databaseService: PrismaClient
+   
     constructor(
-        database: PrismaClient
+        database: PrismaClient,
+        
     ){
-        this.databaseService = database
+        this.databaseService = database;
     }
 
      
@@ -116,22 +119,89 @@ class AuthRepository {
         switch (data.authProvider) {
             case "self":
                 let user = await this.findOneByEmail(data.email!)
-                // check password
-                
+                if(!user){
+                   throw new AuthError(
+                        "We couldn't find an existing user account with this email.",
+                        404,
+                        AuthErrorCode.USER_NOT_FOUND
+                    ) 
+                }
+                const isPasswordCorrect = await bcrypt.compare(data.password!, user.password!)
+                if(!isPasswordCorrect){
+                    throw new AuthError(
+                        "Authentication error. invalid credentials provided",
+                        404,
+                        AuthErrorCode.AUTH_FAILED
+                    ) 
+                }
+                return await this.jwtSign(user)
 
+            case "google":
+                const googleUser = await this.googleAuthProvider(data.authToken!)
+                if(!googleUser) {
+                     throw new AuthError(
+                        "Google could not verify your account",
+                        401,
+                        AuthErrorCode.GOOGLE_AUTH_FAILED
+                    )
+                }
+                user = await this.findOneByEmail(googleUser.email!)
+                if(!user){
+                   throw new AuthError(
+                        "We couldn't find an existing user account with this email, kindle register an account.",
+                        404,
+                        AuthErrorCode.USER_NOT_FOUND
+                    ) 
+                }
+                this.checkAccount(user)
+                // if user is valid, check if this authprovider has been registered for this user
+                // manual find or create
+                try {
+                    await this.databaseService.$transaction(async (prisma) => {
+                        let authProvider = prisma.authProvider.findUnique({
+                            where: {userId_provider:{
+                                userId: user.id,
+                                provider: "google"
+                            }}
+                        })
 
-                await bcrypt.compare(data.password!, user.password!)
-                break;
-        
+                        const authProviderInput:CreateAuthProviderDto = {
+                            userId:user.id,
+                            provider: "google",
+                            providerUserId: googleUser.providerUserId as string
+                        }
+
+                        authProvider =  authProvider ?? this.connectUser(authProviderInput)
+                    })
+                } catch (error) {
+                    throw new AuthError(
+                        "Sever error! sorry we could not log you in due to some unexpected error. Kindly try again.",
+                        500,
+                        AuthErrorCode.INTERNAL_ERROR
+                    ) 
+                }
+                return await this.jwtSign(user)
             default:
-                break;
+                throw new AuthError(
+                    "Unknowns authentication route detected. All associated accounts would be penalised",
+                    404,
+                    AuthErrorCode.MISING_DATA
+                ) 
+                
         }
+
     }
 
-
+    public async findOneById(id: string){
+        const user = await this.databaseService.user.findUnique({
+            where: {
+                id
+            }
+        });
+        return user
+    }
 
     // private methods
-
     public async findOneByEmail(email: string){
          //  check if the provided email is a registered email
         const user = await this.databaseService.user.findFirst({
@@ -141,6 +211,43 @@ class AuthRepository {
         });
         return user
         
+    }
+
+    public async findAll(data: FindManyUsers){
+        const WhereClause:any = {deletedAt: null}
+        if(data.isEmailVerified) WhereClause.isEmailVerified = data.isEmailVerified
+        if(data.status) WhereClause.status = data.status as $Enums.UserStatus
+        if(data.userType) WhereClause.userType = data.userType as $Enums.UserType
+        if(data.deleted) WhereClause.deletedAt = {not: null}
+        // deleted
+    }
+
+    // public async update(id: string, updateDate: ){
+
+    // }
+
+    public async delete(id: string, deletedBy: string, deleteReason:string ){
+        const user = await this.databaseService.user.update({
+            data: {
+                deletedAt: new Date(),
+                deletedBy,
+                deleteReason,
+                status: $Enums.UserStatus.deleted 
+            },
+            where: {
+                id
+            }
+        });
+        return user
+    }
+
+    public async permanentdelete(id: string){
+        const user = await this.databaseService.user.delete({
+            where: {
+                id
+            }
+        });
+        return user
     }
 
     private async connectUser  (data: CreateAuthProviderDto) {
@@ -193,7 +300,7 @@ class AuthRepository {
             const account = await this.databaseService.$transaction(async (prisma) => {
 
                 // Create the user
-                const user = await prisma.user.create({data: newUserInput});
+                const user = await prisma['user'].create({data: newUserInput});
 
                 // Create the related entity based on user type
                 switch (user.userType) {
@@ -261,7 +368,7 @@ class AuthRepository {
         
     }
 
-    private checkAccount (user: {} | undefined){
+    private checkAccount (user: Prisma.UserMinAggregateOutputType){
         if(!user){
             throw new AuthError(
                 "We have no exisitng user account  with this email, kindly register to get started.",
@@ -282,7 +389,36 @@ class AuthRepository {
             )
         }
     }
-}
 
-const authrepository: IAuthRepository = new AuthRepository(prisma)
-export default  authrepository 
+    private async jwtSign (user:Prisma.UserMinAggregateOutputType ){
+        const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET 
+        const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET
+        const JWT_ACCESS_EXPIRES_IN = process.env.JWT_ACCESS_EXPIRES_IN || '1h'
+        const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || '7d'
+
+        try {
+            const accesstoken = jwt.sign({
+                        id:user.id, 
+                        email: user.email, 
+                        gender: user.gender, 
+                        userType: user.userType
+                    }, JWT_ACCESS_SECRET as string, {expiresIn: JWT_ACCESS_EXPIRES_IN as jwt.SignOptions['expiresIn']})
+            const refreshToken = jwt.sign({id:user.id}, JWT_REFRESH_SECRET as string, {expiresIn: JWT_REFRESH_EXPIRES_IN as jwt.SignOptions['expiresIn']})
+
+            // update user with refreshtoken
+            await this.databaseService.user.update({data: {refreshToken}, where: {id: user.id as string}} )
+            return {
+                user,
+                refreshToken,
+                accesstoken
+            }  
+        } catch (error) {
+            throw new AuthError(
+                'An unexpected json error occurred. Please try again later.',
+                500,
+                AuthErrorCode.INTERNAL_ERROR
+            )
+        }
+        
+    }
+}
