3b57864aab453c9b7c1017af2d6b35a1597f6763
Virtue Egerega <egeregav@gmail.com>
2025-06-21 13:16:00 +0100

    Update: implemented login method for authenticatedSessionController

--- src/controllers/auth/AuthenticatedSession.controller.ts
+++ src/controllers/auth/AuthenticatedSession.controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import { authService } from '../../services/authService';
+import Logger from '../../utils/log';
 
 export class AuthController {
   static async register(req: Request, res: Response, next: NextFunction) {
     try {
-      const result = await authService.creatAccount();
-      res.status(201).json(result);
+      const logger = new Logger("Authentication")
+      console.log({req:req.headers})
+      await logger.info(JSON.stringify(req.headers), "authcontroller/register" )
+      return {}
+      // const result = await authService.creatAccount();
+      // res.status(201).json(result);
     } catch (err) {
       next(err);
     }
