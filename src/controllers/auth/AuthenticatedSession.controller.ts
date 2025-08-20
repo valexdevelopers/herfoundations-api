b8b889be4d5c0cdf76319563092ed8e8895d84e1
Virtue Egerega <egeregav@gmail.com>
2025-08-20 15:14:33 +0100

    Update: renamed jenkins file

--- src/controllers/auth/AuthenticatedSession.controller.ts
+++ src/controllers/auth/AuthenticatedSession.controller.ts
@@ -124,6 +124,17 @@ export class AuthController {
         }
     }
 
+    static async changePassword (req: Request, res: Response, next: NextFunction){
+        try {
+            console.log('git here')
+            const user:any = req.user
+            const result = await authService.resendEmailVerificationToken(user.id);
+            res.status(200).json({result})
+
+        } catch (error: any) {
+            next(error)   
+        }
+    } 
     static async update(req: Request, res: Response, next: NextFunction): Promise<any> {
         try {
 
